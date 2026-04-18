f5b683c37f38772c04b93cb0885b01777cf01180
Влад Радченко <vladrma777@gmail.com>
2026-04-18 23:27:28 +0300

    4-reverse с указателем

---
+++ go-demo-4/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [4]int{1, 2, 3, 4}
+	reverse(&a)
+	fmt.Println(a)
+}
+
+func reverse(arr *[4]int) {
+	for index, value := range *arr {
+		(*arr)[len(arr)-1-index] = value
+	}
+}
